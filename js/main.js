bfe816a5dc4b685b57f88daeebc545f852908c3a
Alex Ehrenkranz <alex.ehrenkranz+git@gmail.com>
2023-09-20 02:08:44 +0000

    added id to 'form' element and added event listeners for form submission/image linking

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
+/* eslint-disable no-console */
 const urlInput = document.querySelector('#photo-url');
-
+const imgElement = document.querySelector('img');
 function addImage(event) {
-  const imgSrc = document.querySelector('img');
-  imgSrc.src = urlInput.value;
+  imgElement.src = urlInput.value;
 }
 
 urlInput.addEventListener('input', addImage);
+
+const formElements = document.getElementById('journal');
+const form = document.querySelector('form');
+
+function submitForm(event) {
+  event.preventDefault();
+  const formEntries = {
+    title: formElements.elements.title.value,
+    url: formElements.elements['photo-url'].value,
+    notes: formElements.elements.notes.value,
+    entryId: data.nextEntryId,
+  };
+  data.nextEntryId++;
+  data.entries.unshift(formEntries);
+  imgElement.src = '../images/placeholder-image-square.jpg';
+  form.reset();
+}
+
+form.addEventListener('submit', submitForm);
